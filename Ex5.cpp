4d29e3bd3be7495e6fe5b98e214459e1ca880ef5
JimmyDP <jamesdelpuerto117@gmail.com>
2017-04-11 11:41:53 +1200

    Add files via upload

---
+++ Ex5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	unsigned int A = 60;
+	unsigned int B = 13;
+	
+	printf("A bitshift to the left 4 places = %d\n", A << 4);
+	printf("B bitshift to the right 2 places = %d\n", B >> 2);
+	
+	return 0;
+}
+
